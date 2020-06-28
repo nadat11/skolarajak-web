1e34b3d9207702c978d1908bbfe66ee5fc6fca88
Nada <nadalina.t5@gmail.com>
2020-06-28 18:00:46 +0200

    implementacija II arh.razdvajanje svih komponenti

--- WebContent/vezba-security/pages/adminHomePage.jsp
+++ WebContent/vezba-security/pages/adminHomePage.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
+<%@ page language="java" import="java.util.List, com.skolarajak.model.Vlasnik" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -8,6 +8,44 @@
 </head>
 <body>
 <jsp:include page="menu.jsp" />
-Admin Home Page
+<h3>Admin Home Page</h3>
+<table border="1">
+		<%
+			int pageNumber = 1;
+
+		try {
+			pageNumber = (Integer)request.getAttribute("pageNumber");
+		} catch (NumberFormatException e) {
+
+		}
+       // kada posledi stranu uzima atribut isti je naziv, kastuje se jer getAtribute radi sa objektima 
+		//preuzeli smo sta je servlet propagirao
+	    List<Vlasnik> vlasnici= (List<Vlasnik>)request.getAttribute("listaVlasnika");
+
+		String dataTemplate = "<td>%s</td>";
+
+		if (vlasnici.size() > 0) {
+			for (Vlasnik vlasnik : vlasnici) {
+				out.write("<tr>");
+
+				out.write(String.format(dataTemplate, vlasnik.getIme()));
+
+				out.write(String.format(dataTemplate, vlasnik.getPrezime()));
+				
+				out.write(String.format(dataTemplate, vlasnik.getBrojVozackeDozvole()));
+	
+			 	String brojVozackeDozvole = vlasnik.getBrojVozackeDozvole();
+		
+				out.write("</tr>");
+			}
+		} else {
+			out.write("<div class='no-result>'Nema rezultata </div>");
+		}
+		
+		%>
+		<a class="pages" href="/javaweb/vezba-security/adminHomeServlet.html?p=<%= pageNumber-1 %>"> < </a>
+		<div class="pages">strana <%= pageNumber%> </div>
+		<a class="pages" href="/javaweb/vezba-security/adminHomeServlet.html?p=<%= pageNumber+1 %>"> > </a>
+	</table>
 </body>
 </html>
--- src/com/skolarajak/servlets/AdminHomeServlet.java
+++ src/com/skolarajak/servlets/AdminHomeServlet.java
@@ -1,13 +1,18 @@
 package com.skolarajak.servlets;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.skolarajak.exceptions.dao.ResultNotFoundException;
 import com.skolarajak.model.Roles;
+import com.skolarajak.model.Vlasnik;
+import com.skolarajak.servisi.AdministriranjeVozila;
 
 /**
  * Servlet implementation class AdminHomeServlet
@@ -32,6 +37,28 @@ public class AdminHomeServlet extends BaseAuthorizationServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		if (isAuthorized(request, response)) {
+			
+			Integer pageNumber = 1;
+			try {
+			 pageNumber = Integer.parseInt(request.getParameter("p"));
+			} catch(NumberFormatException e) {
+				pageNumber = 1; //ako nema hvatamo broj nema parametra, pageNumber vratiti 1 stranu
+			}
+			
+			List<Vlasnik> vlasnici = null; // deklaracija liste
+			// insatcirana administracijaVozila
+			AdministriranjeVozila administracijaVozila = new AdministriranjeVozila();// zove servis i koristi pageNumber
+			try {
+				vlasnici = administracijaVozila.dajSveVlasnike(pageNumber); // prikazuje sve vlasnike za pageNumber
+			} catch (ResultNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+
+			}
+			// vraca request
+			request.setAttribute("listaVlasnika", vlasnici);
+			request.setAttribute("pageNumber", pageNumber);
+		
 			// roditeljska metoda
 		request.getRequestDispatcher("/vezba-security/pages/adminHomePage.jsp").forward(request, response);// jsp
 																							// vidi
